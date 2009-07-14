e48b812ff14b486e73cb82127d13c32a1cbca9bd
Gianni Ceccarelli <dakkar@thenautilus.net>
2009-07-14 14:53:13 +0000

    never add a subscription twice; use the URL as key

--- lib/Plagger/Subscription.pm
+++ lib/Plagger/Subscription.pm
@@ -4,11 +4,15 @@ use base qw( Plagger::Update );
 
 sub new {
     my $class = shift;
-    bless { feeds => [], by_tags => {}, by_types => {} }, $class;
+    bless { feeds => [], feed_urls => {}, by_tags => {}, by_types => {} }, $class;
 }
 
 sub add {
     my($self, $feed) = @_;
+
+    return if exists $self->{feed_urls}->{$feed->url};
+    $self->{feed_urls}->{$feed->url}=undef;
+
     push @{ $self->{feeds} }, $feed;
     for my $tag ( @{$feed->tags} ) {
         push @{ $self->{by_tags}->{$tag} }, $feed;
@@ -18,6 +22,9 @@ sub add {
 
 sub delete_feed {
     my($self, $feed) = @_;
+
+    delete $self->{feed_urls}->{$feed->url};
+
     my @feeds = grep { $_ ne $feed } $self->feeds;
     $self->{feeds} = \@feeds;
 }
